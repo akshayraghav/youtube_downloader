e05eddf5ffdd97efbff3d8b04ce778e4ba76d4c2
Akshay raghav <raghav.akki3@gmail.com>
2017-03-21 23:12:45 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Youtube Video Downloader
+Using Flask Framework and pytube library for Python
+
+
+<h3>Setting up Modules and Keys</h3>
+<ul>
+<li>Install Flask by typing the following command on terminal - sudo pip install flask</li>
+<li>Install pytube - sudo pip install pytube</li>
+<li>Create the desired frontend and link the backend script.</li>
+</ul>
+<hr />
+ 
+<h3>Running the application</h3>
+<ul>
+<li>Run the python script</li>
+<li>Now open - http://localhost:5000 on your browser</li>
+<li>The application is displayed.</li>
+</ul>
+
+<h3>Steps to use the application.</h3>
+<ol>
+<li>Paste the url of the desired youtube video in the form.</li>
+<li>Hit "Submit" button.</li>
+<li>Now, the list of all the available quality formats are visible to be chosen from.</li>
+<li>Select the desired video quality.</li>
+<li>Enter the path in your pc where video should be saved.</li>
+<li>Hit "Download".</li>
+<li>You will be directed to another page when the video will be downloaded completely.</li>
+
+<br><img src="https://raw.githubusercontent.com/akshayraghav/youtube_downloader/master/static/screenshots/homepage.png"><br>
+<br><img src="https://raw.githubusercontent.com/akshayraghav/youtube_downloader/master/static/screenshots/step2.png"><br>
+<br><img src="https://raw.githubusercontent.com/akshayraghav/youtube_downloader/master/static/screenshots/step3.png"><br>
+<br><img src="https://raw.githubusercontent.com/akshayraghav/youtube_downloader/master/static/screenshots/final.png"><br>
---
+++ main.py
@@ -0,0 +1,94 @@
+from pytube import YouTube
+from flask import Flask,redirect, url_for,render_template,request
+import requests, random
+requests.packages.urllib3.disable_warnings()
+import ssl
+
+app=Flask(__name__)
+
+yt= None
+rng = None
+list = None
+video = None
+
+def fetch(url):
+    global yt
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        # Legacy Python that doesn't verify HTTPS certificates by default
+        pass
+    else:
+        # Handle target environment that doesn't support HTTPS verification
+        ssl._create_default_https_context = _create_unverified_https_context
+
+    try:
+        yt = YouTube(url)
+    except Exception:
+        return -1
+    else:
+        return yt.get_videos()
+
+@app.route('/')
+def singlemode():
+    return render_template("singlemode.html")
+
+
+
+@app.route('/work', methods=['POST','GET'])
+def work():
+    global list
+    global rng
+    if request.method == 'POST':
+        url_dw = request.form['url']
+        y='https://www.youtube.com/watch'
+        if y in url_dw:
+            list=fetch(url_dw)
+            if list==-1:
+                return render_template('singlemode.html', eval=1)
+            p=len(list)
+            rng=[]
+            for k in range(p):
+                rng.append(k)
+            return render_template('singlemode_down.html', packet=zip(list,rng))
+        else:
+            return render_template('singlemode.html',eval=1)
+
+
+@app.route('/success')
+def success():
+    global video
+    render_template('success.html')
+    video.download("F:/Pytube downloads/")
+    return redirect(url_for('singlemode'))
+
+
+@app.route('/receive', methods=['POST','GET'])
+def receive():
+    global list
+    global video
+    global rng
+    global yt
+    try:
+        choice = int(request.form['category'])
+    except Exception:
+        return render_template('singlemode_down.html',packet=zip(list,rng),eval=1)
+    else:
+        video=list[choice]
+        try:
+            save_at=request.form['save_at']
+            if save_at=='':
+                return render_template('singlemode_down.html',packet=zip(list,rng),eval=2)
+            video.download(save_at)
+        except Exception:
+            num=str(random.randrange(1,10))
+            nm=video.filename
+            name=nm+num
+            yt.set_filename(name)
+            video.download(save_at)
+            return render_template('success.html',vid=video,path=save_at)
+        else:
+            return render_template('success.html', vid=video,path=save_at)
+
+if __name__== '__main__':
+    app.run(debug='true')
